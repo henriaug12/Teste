afbea4829b5262f7460927f89113716911f1b966
henrique <henriaug12@gmail.com>
2024-09-03 13:40:42 -0300

    Adicao de verificaLetra

---
+++ VerificaLetra/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program {
+    public static void Main(string[] args){
+        string entrada;
+        int contador = 0;
+        Console.WriteLine("Informe a string a ser verificada");
+        entrada = Console.ReadLine();
+        foreach(char i in entrada){
+            if(char.ToLower(i) == 'a'){
+                contador++;
+            }
+        }
+        if(contador > 0){
+            Console.WriteLine($"Existem {contador} a's na palavra {entrada}");
+        }
+        else Console.WriteLine($"Não existem a's na palavra {entrada}");
+
+    }
+}
